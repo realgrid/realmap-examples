ea1326088575b1625863e27a88f115ddb1984dc7
Malloc72P <scra1028@gmail.com>
2025-07-15 09:46:46 +0900

    Chore: style 경로 수정

--- angular-sample/src/main.ts
+++ angular-sample/src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import 'realmap/dist/realmap-style.css'
+import 'realmap/realmap-style.css'
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
--- vue-sample/src/main.ts
+++ vue-sample/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import 'realmap/dist/realmap-style.css'
+import 'realmap/realmap-style.css'
 
 createApp(App).mount('#app')
